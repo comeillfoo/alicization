b3bec001fe512266368dd2e2abb468054fb4a852
Come1LLF00 <rayveleaveme34@inbox.ru>
2021-12-08 21:16:47 +0300

    fix: all configs to launch on local; feat: without parameter

--- src/main/java/inc/mimik/alicization/controllers/ResidentsController.java
+++ src/main/java/inc/mimik/alicization/controllers/ResidentsController.java
@@ -1,33 +1,36 @@
 package inc.mimik.alicization.controllers;
 
+import inc.mimik.alicization.entities.ResidentsEntity;
+import inc.mimik.alicization.services.ResidentsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping( "/api" )
 public class ResidentsController {
 
+  private ResidentsService service;
   private static final Logger LOGGER = LoggerFactory.getLogger( ResidentsController.class );
 
-  @GetMapping( path = "/get-resident/{name}" )
-  public ResponseEntity<Object> getResident( @PathVariable String name ) {
+  @GetMapping( path = "/get-residents" )
+  public ResponseEntity<List<ResidentsEntity>> getResidents( ) {
     LOGGER.info( "\ngetResident: started" );
-    LOGGER.info( "getResident: [ name={} ]", name );
 
-    LOGGER.info( "getResident: creating stub" );
-    final Object userStub = new Object() {
-      public final int id = 0;
-      public final String name = "John JohnSon";
-    };
-    LOGGER.info( "getResident: stub created" );
+    LOGGER.info( "getResident: trying to get all residents" );
 
-    LOGGER.info( "getResident: trying to return stub" );
-    return new ResponseEntity<Object>( userStub, HttpStatus.OK );
+    LOGGER.info( "getResident: trying to return all residents" );
+    return new ResponseEntity<List<ResidentsEntity>>( service.getAll(), HttpStatus.OK );
+  }
+
+  public ResidentsController( @Autowired ResidentsService service ) {
+    this.service = service;
   }
 }
